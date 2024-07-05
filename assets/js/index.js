1447443ce379879c8ede035886351eee1807f422
devyusuf <devyusuf01@gmail.com>
2024-07-05 22:50:14 +0100

    added the about and the logo section

--- assets/js/index.js
+++ assets/js/index.js
@@ -6,3 +6,17 @@ document.addEventListener('DOMContentLoaded', () => {
         navMenu.classList.toggle('show');
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const slides = document.querySelectorAll('.text-slide');
+    let currentSlide = 0;
+
+    function showNextSlide() {
+        slides[currentSlide].style.display = 'none';
+        currentSlide = (currentSlide + 1) % slides.length;
+        slides[currentSlide].style.display = 'block';
+    }
+
+    slides[currentSlide].style.display = 'block';
+    setInterval(showNextSlide, 1000);
+});
